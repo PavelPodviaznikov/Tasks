f0c9077a6c633416c15b91611115aa154e0cede2
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-09-18 20:39:26 +0300

    V.2

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
-    $('#button').click(function(){
-       var toAdd = $('input[name=checkListItem]').val();
+   /*$('#button').click(function(){
+       var toAdd = $('#inputItem').val();
        $('.list').append('<div class="item">' + toAdd + '</div>')
-    });
-    $(document).on('click','.item', function(){
+    });*/
+    
+	/*$('div .item').click(function(e){     --------------> Не происходит удаление.
+    	$(e.target).remove();
+	});*/
+ 	var clickCounter; 
+	$(document).on('click','.item', function(){
         $(this).remove();
-    })
+        $('#button').removeAttr('disabled');
+        $('#button').css('background-color', '#cc0000');
+        $('#button').data('counter', clickCounter-1);
+    })  
+	$('#button').data('counter', 1).click(function() {
+    clickCounter = $(this).data('counter');    
+    $(this).data('counter', clickCounter + 1);        
+    if(clickCounter<=10){
+    	var toAdd = $('#inputItem').val();
+        $('.list').append('<div class="item">'+clickCounter+". "+ toAdd + '</div>')
+        console.log(clickCounter);
+
+    }
+     
+    else{
+    	$('#button').attr('disabled', 'disabled');
+    	$('#button').css('background-color', 'grey');
+    	console.log(clickCounter);
+    } 
+    $('#delete_button').on('click', function(){
+    	$('.item').remove();
+    });
+
+
+});
 });
