829595f5b5902e0ffcbafacc5b702dcda1d2e3ec
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-09-19 20:44:36 +0300

    Calculator V.2

--- Calc/calc.js
+++ Calc/calc.js
@@ -1,5 +1,53 @@
 $(document).ready(function() {
-	$('.numbers').on('click', function(){
-		console.log($('.numbers').val());
+	var number = " ";
+	var answer;
+	var newNumber = " ";
+	var operator = " ";
+	var panel = $('#active_panel');
+	panel.text("0");
+
+	$('#numbers > a').not('#clear, #clear_all').on('click', function(){
+		number += $(this).text();
+		panel.text(number);
 	});
+	$('#operators > a').not('#equals').on('click', function(){
+		operator = $(this).text();
+		newNumber = number;
+		number = " ";
+		panel.text(operator);
+	});
+	$('#clear').on('click', function(){
+		number = number.substring(0, number.length-1);
+			panel.text(number);			
+	});
+	$('#clear_all').on('click', function(){
+		number = " ";
+		newNumber = " ";
+		panel.text("0");
+	});
+	$('#equals').on('click', function(){
+		newNumber = parseInt(newNumber);
+		number = parseInt(number);
+		switch(operator){
+			case "+":
+				answer = newNumber + number;
+				break;
+			case "-":
+				answer = newNumber - number;
+				break;
+			case "*":
+				answer = newNumber * number;
+				break;
+			case "/":
+				answer = newNumber/number;
+			default:
+				answer = "error";			
+		}
+
+		answer.toString();
+		panel.text(answer);
+		number = answer;
+
+	});
+
 });
