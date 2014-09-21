9843ad701d7e9c91ef059b81bd708c708349afb3
Pavel Podviaznikov <pavel.podviaznikov@gmail.com>
2014-09-21 20:46:35 +0300

    todo v.5
    undone done tasks

--- New TODO List/script.js
+++ New TODO List/script.js
@@ -1,7 +1,7 @@
 var doneList = [];
 var undoneList = [];
 var clickCounter;
-var global;
+var done_counter = 0;
 
 $(document).ready(function() {
     $('.tabs .tab-links a').on('click', function(e)  {
@@ -13,20 +13,37 @@ $(document).ready(function() {
     });
     $('#button').data('counter', 0).click(function(){
         clickCounter = $(this).data('counter');
-        undoneList[clickCounter] = $('#inputItem').val();
-        $('#undone_tasks').append('<div class="item"><input type="checkbox" class="check">' + undoneList[clickCounter] + '</input></div>');
-        $(this).data('counter', clickCounter+1);
-    	$('#undone_tasks_counter').text(undoneList.length);
+        if($('#inputItem').val()===""){
+        	 alert("Oops, try again!");
+        }
+        else{
+        	undoneList[clickCounter] = $('#inputItem').val();
+	        $('#undone_tasks').append('<div class="item">' + undoneList[clickCounter] + '</div><hr>');
+	        $(this).data('counter', clickCounter+1);
+	    	$('#undone_tasks_counter').text(undoneList.length);
+	    	console.log($('#inputItem').val());
+        } 
     });
     $('#delete_button').on('click', function(){
         $('.item').remove();
         undoneList = [];
+        doneList=[];
+        $('hr').remove();
         $('#button').data('counter', 0);
     	$('#undone_tasks_counter').text(undoneList.length);
+    	$('#done_tasks_counter').text(doneList.length);
     });
     $(document).on('click','.item', function(){
-        global = $(this).val();
-        console.log(global);
+        var task = $(this).text();
+        for(var i=0; i<undoneList.length; i+=1){
+        	if(undoneList[i]===task){
+        		doneList.push(undoneList[i]);
+        		$('#done_tasks').append('<div class="item">' + doneList[done_counter] + '</div><hr>');
+        		done_counter+=1;
+        	}
+        }
+        
+        $('#done_tasks_counter').text(doneList.length);
     }); 
     /*$('#counter_button').on('click', function(){
     	$('#done_tasks_counter').text(doneList.length);
